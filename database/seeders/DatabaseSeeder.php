1942e988743044d665467ea3800c19ee4d429b9f
rizkisahat996 <rizkisitumeang77@gmail.com>
2022-04-21 10:50:12 +0700

    w

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,71 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
+
+        // User::create([
+        //     'name' => 'Rizki Sahat',
+        //     'email' => 'rizkisahat@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Fera Kurniawan',
+        //     'email' => 'feraa@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+
+
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web-Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Gaming',
+            'slug' => 'Gaming'
+        ]);
+
+        Post::factory(20)->create();
+
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'exert' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur explicabo consectetur odio facilis quis dolorem itaque nesciunt, laborum adipisci dolores?</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem harum tenetur ratione? Officiis exercitationem, minus nobis rerum consequuntur ducimus culpa provident quae quos odio ea expedita qui totam sint eius modi? Quaerat nihil, porro officia sint inventore magnam reiciendis pariatur assumenda dolore fugit possimus ea obcaecati odit. Ea, velit assumenda?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae soluta ratione dolorem et enim quisquam commodi quos odit laudantium, culpa ducimus nulla asperiores ipsum voluptate dolore sequi molestias qui facilis incidunt officia doloremque maiores nemo aut. Id eligendi quis nulla, animi tempore quod totam odit. Animi libero pariatur hic voluptatum.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum odio earum laborum eaque aut facilis sequi assumenda, corrupti, natus esse qui? Officiis, saepe! Odit atque, quisquam, voluptas voluptates a vitae suscipit nostrum eligendi officiis aperiam molestiae totam mollitia iste inventore cum, nulla optio provident ipsum hic! Rerum, tempora expedita!</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul kedua',
+        //     'slug' => 'judul-ke-dua',
+        //     'exert' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur explicabo consectetur odio facilis quis dolorem itaque nesciunt, laborum adipisci dolores?</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem harum tenetur ratione? Officiis exercitationem, minus nobis rerum consequuntur ducimus culpa provident quae quos odio ea expedita qui totam sint eius modi? Quaerat nihil, porro officia sint inventore magnam reiciendis pariatur assumenda dolore fugit possimus ea obcaecati odit. Ea, velit assumenda?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae soluta ratione dolorem et enim quisquam commodi quos odit laudantium, culpa ducimus nulla asperiores ipsum voluptate dolore sequi molestias qui facilis incidunt officia doloremque maiores nemo aut. Id eligendi quis nulla, animi tempore quod totam odit. Animi libero pariatur hic voluptatum.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum odio earum laborum eaque aut facilis sequi assumenda, corrupti, natus esse qui? Officiis, saepe! Odit atque, quisquam, voluptas voluptates a vitae suscipit nostrum eligendi officiis aperiam molestiae totam mollitia iste inventore cum, nulla optio provident ipsum hic! Rerum, tempora expedita!</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 2
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul ketiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'exert' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur explicabo consectetur odio facilis quis dolorem itaque nesciunt, laborum adipisci dolores?</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem harum tenetur ratione? Officiis exercitationem, minus nobis rerum consequuntur ducimus culpa provident quae quos odio ea expedita qui totam sint eius modi? Quaerat nihil, porro officia sint inventore magnam reiciendis pariatur assumenda dolore fugit possimus ea obcaecati odit. Ea, velit assumenda?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae soluta ratione dolorem et enim quisquam commodi quos odit laudantium, culpa ducimus nulla asperiores ipsum voluptate dolore sequi molestias qui facilis incidunt officia doloremque maiores nemo aut. Id eligendi quis nulla, animi tempore quod totam odit. Animi libero pariatur hic voluptatum.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum odio earum laborum eaque aut facilis sequi assumenda, corrupti, natus esse qui? Officiis, saepe! Odit atque, quisquam, voluptas voluptates a vitae suscipit nostrum eligendi officiis aperiam molestiae totam mollitia iste inventore cum, nulla optio provident ipsum hic! Rerum, tempora expedita!</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul keempat',
+        //     'slug' => 'judul-ke-empat',
+        //     'exert' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur explicabo consectetur odio facilis quis dolorem itaque nesciunt, laborum adipisci dolores?</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem harum tenetur ratione? Officiis exercitationem, minus nobis rerum consequuntur ducimus culpa provident quae quos odio ea expedita qui totam sint eius modi? Quaerat nihil, porro officia sint inventore magnam reiciendis pariatur assumenda dolore fugit possimus ea obcaecati odit. Ea, velit assumenda?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae soluta ratione dolorem et enim quisquam commodi quos odit laudantium, culpa ducimus nulla asperiores ipsum voluptate dolore sequi molestias qui facilis incidunt officia doloremque maiores nemo aut. Id eligendi quis nulla, animi tempore quod totam odit. Animi libero pariatur hic voluptatum.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum odio earum laborum eaque aut facilis sequi assumenda, corrupti, natus esse qui? Officiis, saepe! Odit atque, quisquam, voluptas voluptates a vitae suscipit nostrum eligendi officiis aperiam molestiae totam mollitia iste inventore cum, nulla optio provident ipsum hic! Rerum, tempora expedita!</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PostController;
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -67,11 +68,15 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories/{category:slug}', function(Category $category)
 {
-    return view('category', [
-        'title' => $category->name,
-        'slug' => $category->slug,
-        'exert' => $category->exert,
-        'post' => $category->posts,
-        'category'=> $category->name
+    return view('posts', [
+        'title' => "Post by Category : $category->name",
+        'posts' => $category->post
+    ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author){
+    return view('posts', [
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->post,
     ]);
 });
