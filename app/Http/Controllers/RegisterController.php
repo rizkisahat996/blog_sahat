ab527e2fedb4e4378b90ce9aaebd88aba63baf13
rizkisahat996 <rizkisitumeang77@gmail.com>
2022-04-27 11:55:36 +0700

    perbaikan dikit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
 
 class RegisterController extends Controller
 {
